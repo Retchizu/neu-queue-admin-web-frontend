9fb5dc852b16304e97f2d8a1007559ce2a1142a5
Romnoel Edralin Petracorta <petracorta.rmnl@gmail.com>
2025-10-05 09:41:55 +0800

    feat (sidebar): created an empty sidebar.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
+import { AppSidebar } from "@/components/app-sidebar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -24,11 +26,17 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+      <SidebarProvider>
+        <AppSidebar />
+        <body
+          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        >
+          <main>
+            <SidebarTrigger />
+            {children}
+          </main>
+        </body>
+      </SidebarProvider>
     </html>
   );
 }

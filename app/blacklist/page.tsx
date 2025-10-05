86fb6be76e56f9ad26138c11791fe4c1b4c31ef0
Romnoel Edralin Petracorta <petracorta.rmnl@gmail.com>
2025-10-05 09:17:32 +0800

    feat(routes): added appropriate routes.

---
+++ app/blacklist/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      blacklist
+    </div>
+  )
+}
+
+export default page
---
+++ app/review-pending-accounts/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      review-pending
+    </div>
+  )
+}
+
+export default page
